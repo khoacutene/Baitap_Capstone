e059bff2cde46ceada05ee660c4772b6ed53a28a
khoacutene <dangtienkhoa11192005@gmail.com>
2024-08-18 23:48:39 +0700

    them tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        stColor: "red",
+        secondColor: "#d3dce6",
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
