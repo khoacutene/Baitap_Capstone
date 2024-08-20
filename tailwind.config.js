eebf7382b8e7775a7828d3571fc56d36f7824a1c
khoacutene <dangtienkhoa11192005@gmail.com>
2024-08-20 21:20:30 +0700

    text github

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {
-      backgroundColor:{
-        stColor: "red",
-        secondColor: "#d3dce6",
-      },
-      
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
 
