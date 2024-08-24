b1f5fb286bfde956bdccc91a5b188c243732bb23
khoacutene <dangtienkhoa11192005@gmail.com>
2024-08-24 14:10:14 +0700

    nop bai lan 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '4.5xl': '44px',
+        '2.5xl': '26px',
+        '44xl': '44px',
+      },
+      margin: {
+        '1.875': '1.875rem',
+      }
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
